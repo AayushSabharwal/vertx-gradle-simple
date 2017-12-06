94c2e8349086699f28eb316e04b1753624b70a6a
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2017-12-06 09:00:30 -0800

    Updated strings.java
    Now contains strings as variables instead of functions to print

--- src/main/java/io/vertx/example/strings.java
+++ src/main/java/io/vertx/example/strings.java
@@ -1,19 +1,12 @@
 package io.vertx.example;
 
 import io.vertx.core.Vertx;
-
 /**
  * @author Aayush Sabharwal
  */
 
 public class strings{
-    public static void localhostPort()
-    {
-        System.out.println("http://localhost:8080/");
-    }
+    public lhostAddress = "http://localhost:8080/";
+    public Err_couldNotListen = "Failed to listen on port 8080";
 
-    public static void portError_CouldNotListen()
-    {
-        System.err.println("Failed to listen on port 8080");
-    }
 }
