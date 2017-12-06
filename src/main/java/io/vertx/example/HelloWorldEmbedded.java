cd40b0f667daa126e09aaf951875d69c7ad22d4c
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2017-12-06 08:59:30 -0800

    Updated class HelloWorldEmbedded
    Class now interacts with strings.java for string retrieval

--- src/main/java/io/vertx/example/HelloWorldEmbedded.java
+++ src/main/java/io/vertx/example/HelloWorldEmbedded.java
@@ -1,23 +1,22 @@
 package io.vertx.example;
 
 import io.vertx.core.Vertx;
-
-
 /**
  * @author <a href="http://tfox.org">Tim Fox</a>
  */
 public class HelloWorldEmbedded {
 
   public static void main(String[] args) {
+
     // Create an HTTP server which simply returns "Hello World!" to each request.
     Vertx.vertx()
       .createHttpServer()
       .requestHandler(req -> req.response().end("Hello World!"))
       .listen(8080, handler -> {
         if (handler.succeeded()) {
-          strings.localhostPort();
+            System.out.println(strings.lhostAddresss);
         } else {
-          strings.portError_CouldNotListen();
+          System.err.println(strings.Err_couldNotListen);
         }
       });
   }
